0c156c46de287c3ff7319ed23dd39fb8d1929201
Misha373 <misasapka1@gmail.com>
2024-09-24 23:19:27 +0300

    project is are ready

--- HW10.3/Program.cs
+++ HW10.3/Program.cs
@@ -1,1 +1,5 @@
 ﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+string Temp = "Milk, Water, Tea, Coffe, Juice.";
+string replaced = Temp.Replace(" ", "");
+Console.WriteLine($"Рядок до зміни: {Temp}");
+Console.WriteLine($"Рядок після зміни: {replaced}");
